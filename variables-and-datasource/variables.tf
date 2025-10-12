16775a529abf798ef712a4a1726bdc74dc711129
oluwolewilliam <oluwolewilliams3@gmail.com>
2025-10-12 16:59:05 +0100

    demo

---
+++ variables-and-datasource/variables.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+variable "aws_region" {
+  description = "region in whcich to create resources"
+  type        = string
+  default     = "us-east-1"
+
+}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+
+
+}
+variable "instance_keypair" {
+  description = "EC2 instance keypair name"
+  type        = string
+  default     = "terraform-keypair1"
+
+
+
+
+}
